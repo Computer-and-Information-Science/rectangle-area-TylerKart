29ce17e24cfd00cc46044725b7d255d8eca525be
TylerKart <tyler.karthaeuser@student.northampton.edu>
2026-02-01 00:02:54 +0000

    Output correct

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
+  double Area;
+  int Perimeter;
 
+
+    Area = length * width;
+    Perimeter = 2 * (length + width);
+
+  cout<< "Rectangle Properties:"<< endl;
+  cout<< "Length = "<< length << endl;
+  cout<< "Width = "<< width << endl;
+  cout<< "Area = "<< Area << endl;
+  cout<< "Perimeter = "<< Perimeter << endl; 
 
 }
